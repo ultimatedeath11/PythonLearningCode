13d6acfcf2b66665352da3a708ab79375b8955d6
Noah Franck <ultimatedeath11@icloud.com>
2017-03-28 15:39:38 -0500

    re-created my python repo
    repo for coding and learning python.

---
+++ ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+#above is import statment that refrences what is in the terminal
+
+script, first, second, third = argv
+#assigns the varialbes to arguments that I list in the terminal
+
+print "The script is called:", first
+print "Your first variable is:", second
+print "Your second variable is",third
