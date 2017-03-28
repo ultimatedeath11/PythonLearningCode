76de1adaaa0ee3c7a4cbcb2c8b441feeb838edd6
Noah Franck <ultimatedeath11@icloud.com>
2017-03-28 16:41:53 -0500

    ex 14 changes
    I have fixed issues in the code in which it would not run, simple bugs.
    mainly typo’s

--- ex14.py
+++ ex14.py
@@ -1,18 +1,20 @@
-from sys imprt argvs
+from sys import argv
 
 script, user_name = argv
-prompt = '>'
+#script is predetermined to be the name of the file
+#username is the one thing to unpack 
+prompt = '> '
 
 print "Hi %s, I'm the %s script." % (user_name, script)
 print "I'd like to ask you a few questions."
 print "Do you like me %s?" % user_name
 likes = raw_input(prompt)
 
-print "Where do you live %s" user_name
+print "Where do you live %s" % user_name
 lives = raw_input(prompt)
 
 print "what kind of computer do you have?"
-comptuer = raw_input(prompt)
+computer = raw_input(prompt)
 
 print """
 Alright, so you said %r about liking me.
