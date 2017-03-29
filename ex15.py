f3dbb98e04e866be6f431330a4297c4308ca91eb
Noah Franck <ultimatedeath11@icloud.com>
2017-03-29 10:57:05 -0500

    +ed comments
    Just commented what it is doing, but I will need to go back and re-do
    it.

--- ex15.py
+++ ex15.py
@@ -3,9 +3,16 @@ from sys import argv
 script, filename = argv
 
 txt = open(filename)
+#creates variable and then prints it into the console
+#the variable stores the .txt name so it should in theory open any variable
+
+#behind the scenes making a 'file object'
+#
+
 
 print "Here's your file %r:" % filename
 print txt.read()
+
 
 print"Type the filename again:"
 file_again = raw_input("> ")
